8ce11e0fe78fa9b7ed7762aa2e7171cacd0b873e
Carlos Villuendas <carlosvillu@gmail.com>
2020-10-31 20:38:09 +0100

    chore(META): Eject CRA

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import * as serviceWorker from './serviceWorker'
 
 import {Pajarito} from './domain'
 
@@ -21,8 +20,3 @@ async function render() {
 }
 
 render()
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
