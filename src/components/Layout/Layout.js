9eaa6c1c6d9177043bcc1da368597924034c9e21
Gabriel Salas <ggsalas@gmail.com>
2021-01-02 09:38:25 -0300

    feat(): add menu to logout user

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
@@ -8,13 +8,24 @@ import Container from '@material-ui/core/Container'
 import PropTypes from 'prop-types'
 import s from './Layout.module.scss'
 import { useHistory } from 'react-router-dom'
+import MenuItem from '@material-ui/core/MenuItem'
+import Menu from '@material-ui/core/Menu'
 
 import { Global } from '../../contexts/global'
 
 export function Layout({ name, userName, children }) {
   const { domain } = useContext(Global)
   const history = useHistory()
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
 
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
   async function handleLogout() {
     const [error] = await domain.get('logoutUserUseCase').execute()
 
@@ -30,15 +41,39 @@ export function Layout({ name, userName, children }) {
         <Container maxWidth="sm">
           <Toolbar className={s.layout__toolbar} disableGutters>
             <Typography variant="h6">
-              <span onClick={handleLogout}>{name}</span>
+              <span>{name}</span>
             </Typography>
 
-            <Button startIcon={<AccountCircle />} color="inherit">
-              <Typography variant="body1">{userName}</Typography>
-            </Button>
+            <div>
+              <Button
+                startIcon={<AccountCircle />}
+                color="inherit"
+                onClick={handleMenu}
+              >
+                <Typography variant="body1">{userName}</Typography>
+              </Button>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleLogout}>Logout</MenuItem>
+              </Menu>
+            </div>
           </Toolbar>
         </Container>
       </AppBar>
+
       <div className={s.content}>{children}</div>
     </div>
   )
